c9c2f47651fa3611e6701848f20e3bc491caa455
anfalfadhil <anfalfadhil90@gmail.com>
2022-08-01 13:39:36 -0400

    connected to atals mongodb and created collection customers

---
+++ src/JSDollarsBank.js
@@ -0,0 +1,42 @@
+console.log("Hello Dollars")
+
+
+const {MongoClient} = require("mongodb")
+
+async function main () {
+    const uri="mongodb+srv://mongodb22:mongodb22@cluster0.pc6ddwv.mongodb.net/?retryWrites=true&w=majority"
+    const client = new MongoClient(uri);  
+    try {
+        await client.connect();
+        createDbs(client);
+        await listDatabases(client);
+    } catch (e) {
+        console.log(e)
+    } finally {
+        await client.close();
+    }
+    
+
+
+}
+
+main().catch(console.error);
+
+
+async function listDatabases(client) {
+
+    const  databasesList = await client.db().admin().listDatabases();
+    console.log("databases")
+    
+    databasesList.databases.forEach(db => {
+        console.log(`- ${db.name}`)
+    })
+
+}
+
+function createDbs (client) {
+    client.db().createCollection("customers", function(err, result) {
+    if (err) throw err;        
+    console.log("Collection is created!");
+    });
+}
